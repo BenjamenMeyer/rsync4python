1bf97a3a6f0abfd11bb713ceb31edca56508f9e8
BenjamenMeyer <ben.meyer@rackspace.com>
2015-02-12 22:40:57 +0000

    Bug Fix: Distributables
    - Previously the repo was having issues with building the distributables for pipy
      This fixes it and makes them work, 'tox' is not available :)

--- setup.py
+++ setup.py
@@ -4,11 +4,46 @@ from os import path
 from setuptools import setup, find_packages, Extension
 import sys
 
-REQUIRES = ['cython']
+REQUIRES = []
 DESCRIPTION = 'rsync access for python'
 
-from Cython.Build import cythonize
-from Cython.Distutils import build_ext
+# Note: In order to ship this you have to run the following
+#       commands in order:
+#
+# 1. build the C files
+#       python setup.py build_ext --inplace
+# 2. build the distributable
+#       python setup.py sdist
+#       python setup.py bdist
+# 3. upload the files
+#       twine upload dist/...
+#    or
+#       python setup.py sdist upload
+#       python setup.py bdist upload
+#
+precompiled_extensions = [
+    Extension('rsync4python.rsync',
+              sources=['rsync4python/rsync.c'],
+              libraries=['rsync'])
+]
+
+extensions = None
+
+try:
+    from Cython.Build import cythonize
+    from Cython.Distutils import build_ext
+
+    cython_extensions = [
+        Extension('rsync4python.rsync',
+                  ['rsync4python/rsync.pyx'],
+                  libraries=['rsync'])
+    ] 
+    extensions = cythonize()
+
+except ImportError:
+    extensions = precompiled_extensions
+    
+    
 
 setup(
     name='rsync4python',
@@ -20,8 +55,7 @@ setup(
     author_email='ben.meyer@rackspace.com',
     install_requires=REQUIRES,
     zip_safe=False,
-    cmdclass = {'build_ext': build_ext },
-    ext_modules=cythonize([Extension('rsync4python.rsync', ['rsync4python/rsync.pyx'], libraries=['rsync'])]),
+    ext_modules=extensions,
     packages=find_packages(exclude=['tests',
                                     'rsync4python/tests*']),
     entry_points={
